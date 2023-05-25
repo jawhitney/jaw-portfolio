7a63f4a3c02868ba01e20fec8c2a4c889f91ffc0
Jonathan Whitney <jonathan.whitney@filo.co>
2023-05-25 14:29:26 -0400

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+// import './App.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 import Home from './components/routing/Home'
 import Projects from './components/routing/Projects'
