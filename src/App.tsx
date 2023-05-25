9538e85a64269f6a1a700b71a506db64fe7d2a07
Jonathan Whitney <jonathan.whitney@filo.co>
2023-05-25 15:10:06 -0400

    Set MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
+import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import './App.css'
+import './App.css'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-import Home from './components/routing/Home'
-import Projects from './components/routing/Projects'
-import Resume from './components/routing/Resume'
-import About from './components/routing/About'
-import Contact from './components/routing/Contact'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Switch } from '@mui/material'
+
+import Home from './Components/routing/Home'
+import Projects from './Components/routing/Projects'
+import Resume from './Components/routing/Resume'
+import About from './Components/routing/About'
+import Contact from './Components/routing/Contact'
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false)
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  })
+
+  const handleChangeTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDarkMode(event.target.checked)
+  }
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Switch
+        checked={darkMode}
+        onChange={handleChangeTheme}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -23,7 +46,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </ThemeProvider>
   )
 }
 
