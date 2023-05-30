b7f896f21331ae7b35dac59483be8b7e04114b50
Jonathan Whitney <jonathan.whitney@filo.co>
2023-05-30 14:22:41 -0400

    Updated layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import logo from './assets/jw-logo.svg'
 
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Switch } from '@mui/material'
+import { FormControlLabel, Grid, Switch } from '@mui/material'
 
+import Header from './Components/Header'
 import Home from './Components/routing/Home'
 import Projects from './Components/routing/Projects'
 import Resume from './Components/routing/Resume'
@@ -32,20 +34,30 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Switch
-        checked={darkMode}
-        onChange={handleChangeTheme}
-        inputProps={{ 'aria-label': 'controlled' }}
-      />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <main>
+        <Header />
+        <section>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </BrowserRouter>
+        </section>
+        <div className="darkMode">
+          <FormControlLabel
+            control={<Switch
+              checked={darkMode}
+              onChange={handleChangeTheme}
+              inputProps={{ 'aria-label': 'dark mode' }}
+            />}
+            label="Dark Mode"
+          />
+        </div>
+      </main>
     </ThemeProvider>
   )
 }
